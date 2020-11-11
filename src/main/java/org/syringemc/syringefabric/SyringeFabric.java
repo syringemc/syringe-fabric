b126def46a048755762bff9183eda2487b76bd2c
uTen2c <uten2c@gmail.com>
2020-11-11 16:58:41 +0900

    Initial commit

---
+++ src/main/java/org/syringemc/syringefabric/SyringeFabric.java
@@ -0,0 +1,10 @@
+package org.syringemc.syringefabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SyringeFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
