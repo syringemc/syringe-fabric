d19eaab2804f4c76db6461326fe41ea4cd3bc7a8
krmn <38986306+krmn08@users.noreply.github.com>
2020-11-13 23:23:07 +0900

    Add initialization code

--- src/main/java/org/syringemc/syringefabric/SyringeFabric.java
+++ src/main/java/org/syringemc/syringefabric/SyringeFabric.java
@@ -1,10 +1,16 @@
 package org.syringemc.syringefabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.server.MinecraftServer;
+import org.syringe.api.SyringeAPI;
 
 public class SyringeFabric implements ModInitializer {
+    public static MinecraftServer SERVER;
+
     @Override
     public void onInitialize() {
-
+        SyringeAPI.init(new APIImpl());
+        ServerLifecycleEvents.SERVER_STARTING.register((server) -> SERVER = server);
     }
 }
